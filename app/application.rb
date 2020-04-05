e308a5aea5f51b43af17ea04c1073d1a3999b5bb
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-05 16:08:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.detect { |i| i.name == item_name }
       if item 
+        resp.write item.price
         
   end
 end
