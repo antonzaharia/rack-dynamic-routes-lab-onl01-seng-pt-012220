f2a4aaa1f0275cd06abf8ec5a01f26453b7f640e
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-05 16:08:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
     if req.path.match(/items\/.+/)
       item_name = req.path.split("/items/").last
       item = @@items.detect { |i| i.name == item_name }
-      if 
+      if item 
+        
   end
 end
